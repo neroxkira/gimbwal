536cc8077986add61faf9e4d61f9551441b6828d
1dleocan <ocan6112@gmail.com>
2025-02-24 01:06:45 +0800

    script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function getFarPosition(containerRect, windowWidth, windowHeight, buttonWidth, b
 }
 
 yesBtn.addEventListener('click', () => {
-    title.innerHTML = "HORE!!, yaudaa mau eskrimm apaaa";
+    title.innerHTML = "HORE!!, KITA FIX BALIKANN! CIHUYY!";
     document.querySelector('img').remove(); 
     noBtn.style.display = 'none';
     yesBtn.style.display = 'none';
@@ -134,7 +134,7 @@ noBtn.addEventListener('click', () => {
         title.innerHTML = messages[noCount - 1].text;
         document.querySelector('img').src = messages[noCount - 1].image;
     } else {
-        title.innerHTML = "TAPI BOONG HEHEHE";
+        title.innerHTML = "HOREE!!";
         if (!noBtn.classList.contains('running')) {
             noBtn.classList.add('running');
         }
